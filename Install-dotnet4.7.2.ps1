d5131027bfba72c9eb82e3d28dd8911508891eb1
hagai maya <hagaim@axonize.com>
2018-08-20 14:37:38 +0300

    added script dotnet4.7 installation

---
+++ Install-dotnet4.7.2.ps1
@@ -0,0 +1,1 @@
+powershell.exe -command 'wget http://go.microsoft.com/fwlink/?LinkId=863265 -UseBasicParsing -OutFile NDP472-KB4054530-x86-x64-AllOS-ENU.exe; .\NDP472-KB4054530-x86-x64-AllOS-ENU.exe /q'
